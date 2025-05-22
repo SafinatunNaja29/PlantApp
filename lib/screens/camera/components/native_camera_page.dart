08383bf69b45dced48c3661c7803a68a74c0b0ef
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-23 00:03:48 +0700

    Menambahkan struktur dasar halaman kamera

---
+++ lib/screens/camera/components/native_camera_page.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}

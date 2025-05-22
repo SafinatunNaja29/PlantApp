729bbbfeeead3faa092af9a4d47fafd3bdab6cfb
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-22 23:06:16 +0700

    Mendesain widget judul dengan efek garis bawah berwarna dan padding

--- lib/screens/home/components/title_with_more_bbtn.dart
+++ lib/screens/home/components/title_with_more_bbtn.dart
@@ -46,4 +46,35 @@ class TitleWithCustomUnderline extends StatelessWidget {
   }) : super(key: key);
 
   final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 24,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: const EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }

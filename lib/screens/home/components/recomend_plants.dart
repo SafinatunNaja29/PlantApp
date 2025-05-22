e6b5bc1de445e351e2c2641084c1d98b5a52e8c1
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-22 22:53:51 +0700

    Mengimplementasikan gesture dan styling pada bagian bawah kartu tanaman

--- lib/screens/home/components/recomend_plants.dart
+++ lib/screens/home/components/recomend_plants.dart
@@ -76,6 +76,32 @@ class RecomendPlantCard extends StatelessWidget {
         left: kDefaultPadding,
         top: kDefaultPadding / 2,
         bottom: kDefaultPadding * 2.5,
+      ),
+      width: size.width * 0.4,
+      child: Column(
+        children: <Widget>[
+          Image.asset(image),
+          GestureDetector(
+            onTap: press,
+            child: Container(
+              padding: const EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ]
       )
     );
   }

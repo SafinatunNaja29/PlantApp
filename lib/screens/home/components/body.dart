489f55553d8d8c589e72732a0601b93d3c245cf3
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-22 12:29:01 +0700

    Membuat elemen UI di bawah header untuk search bar

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -21,6 +21,27 @@ class Body extends StatelessWidget {
                   ),
                 ),
               ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                  height: 54,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: kPrimaryColor.withOpacity(0.23),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         )

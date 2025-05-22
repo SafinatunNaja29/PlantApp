1393715e4caa78c05fa23c01284128a7f82070f0
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-22 23:21:15 +0700

    Membuat layout ImageAndIcons dengan tombol back dan deretan IconCard vertikal

--- lib/screens/details/components/image_and_icons.dart
+++ lib/screens/details/components/image_and_icons.dart
@@ -11,4 +11,50 @@ class ImageAndIcons extends StatelessWidget {
   }) : super(key: key);
 
   final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: kDefaultPadding * 3),
+      child: SizedBox(
+        height: size.height * 0.8,
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: kDefaultPadding * 3,
+                ),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: kDefaultPadding,
+                        ),
+                        child: IconButton(
+                          icon: SvgPicture.asset(
+                            "assets/icons/back_arrow.svg",
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    const IconCard(icon: "assets/icons/sun.svg"),
+                    const IconCard(icon: "assets/icons/icon_2.svg"),
+                    const IconCard(icon: "assets/icons/icon_3.svg"),
+                    const IconCard(icon: "assets/icons/icon_4.svg"),
+                  ],
+                ),
+              ),
+            ),
+          ]
+        )
+      )
+    );
+  }
 }

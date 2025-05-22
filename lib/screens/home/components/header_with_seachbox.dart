5e229c5818ba9b6394ba36aba1220903ac260444
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-22 22:36:41 +0700

    Menambahkan container pencarian di bawah header dengan shadow

--- lib/screens/home/components/header_with_seachbox.dart
+++ lib/screens/home/components/header_with_seachbox.dart
@@ -45,7 +45,28 @@ class HeaderWithSearchBox extends StatelessWidget {
               ],
             ),
           ),
-
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              height: 54,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23),
+                  ),
+                ],
+              ),
+            )
+          )
         ],
       ),
     );

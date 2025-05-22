bec39233b6b6f01001b0d7921084355de924be5f
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-23 00:06:16 +0700

    Menambahkan logika inisialisasi kamera pada CameraPage

--- lib/screens/camera/components/native_camera_page.dart
+++ lib/screens/camera/components/native_camera_page.dart
@@ -9,3 +9,20 @@ class CameraPage extends StatefulWidget {
   @override
   State<CameraPage> createState() => _CameraPageState();
 }
+
+class _CameraPageState extends State<CameraPage> {
+  late List<CameraDescription> _cameras;
+  CameraController? _controller;
+  int _selectedCameraIdx = 0;
+  FlashMode _flashMode = FlashMode.off;
+  double _zoom = 1.0;
+  double _minZoom = 1.0;
+  double _maxZoom = 1.0;
+  bool _isZoomSupported = false;
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    _initializeCamera();
+  }

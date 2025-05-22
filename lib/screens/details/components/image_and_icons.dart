1ccfc33b57a1b4423b99b58880f240a21c619b70
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-22 23:18:21 +0700

    Menambahkan struktur dasar ImageAndIcons

---
+++ lib/screens/details/components/image_and_icons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../constants.dart';
+import 'icon_card.dart';
+
+class ImageAndIcons extends StatelessWidget {
+  const ImageAndIcons({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+}

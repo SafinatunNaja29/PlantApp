e1cf63263727c77abb34a3445cff4e08e4db5735
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-22 23:11:52 +0700

    Membuat widget IconCard

---
+++ lib/screens/details/components/icon_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../constants.dart';
+
+class IconCard extends StatelessWidget {
+  const IconCard({
+    Key? key,
+    required this.icon,
+  }) : super(key: key);
+
+  final String icon;
+}

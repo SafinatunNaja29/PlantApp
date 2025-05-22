8291614ecbed5bfc776796520d74fc0601942316
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-22 22:44:03 +0700

    Menambahkan daftar tanaman rekomendasi

--- lib/screens/home/components/recomend_plants.dart
+++ lib/screens/home/components/recomend_plants.dart
@@ -4,4 +4,29 @@ import '../../../constants.dart';
 
 class RecomendsPlants extends StatelessWidget {
   const RecomendsPlants({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          RecomendPlantCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailsScreen(),
+                ),
+              );
+            },
+          ),
+        ]
+      )
+    );
+  }
 }

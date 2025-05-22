3f1078572363fe71ed1eb365f53e57864287ce6a
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-05-22 23:25:52 +0700

    Menambahkan tampilan gambar utama dengan styling

--- lib/screens/details/components/image_and_icons.dart
+++ lib/screens/details/components/image_and_icons.dart
@@ -52,9 +52,31 @@ class ImageAndIcons extends StatelessWidget {
                 ),
               ),
             ),
-          ]
-        )
-      )
+            Container(
+              height: size.height * 0.8,
+              width: size.width * 0.75,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(63),
+                  bottomLeft: Radius.circular(63),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(0, 10),
+                    blurRadius: 60,
+                    color: kPrimaryColor.withOpacity(0.29),
+                  ),
+                ],
+                image: const DecorationImage(
+                  alignment: Alignment.centerLeft,
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/img.png"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
